18998f52fff59c66ff216899b9328b5df0cc7a0d
giz1234 <105382412+giz1234@users.noreply.github.com>
2022-05-28 02:02:39 +0300

    Add files via upload

---
+++ hw2/src/test/java/StepDefinitions/ProductPageSteps.java
@@ -0,0 +1,23 @@
+package StepDefinitions;
+
+import Pages.ProductPage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProductPageSteps {
+
+    @Then("Product Page in acildigi kontrol edilir")
+    public void productPageInAcildigiKontrolEdilir() {
+        ProductPage.checkProductPage();
+
+
+    }
+
+    @When("Favoriye tiklanir")
+    public void favoriyeTiklanir() {
+        ProductPage.clickFav();
+        
+    }
+
+
+}
